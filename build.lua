4ea9a8b4c911bfb972926b138754140062954a84
kprussing <kprussing74@gmail.com>
2020-09-17 23:19:05 -0400

    Add upload configuration details

--- build.lua
+++ build.lua
@@ -14,3 +14,23 @@ for _, example in pairs(demofiles) do
     table.insert(tdslocations, docdir .. example)
 end
 
+-- Set the configuration details
+uploadconfig = {
+    -- Identification
+    author = "Keith F Prussing",
+    uploader = "Keith F Prussing",
+    email = "kprussing74@gmail.com",
+
+    -- URLs
+    home = "https://github.com/kprussing/secretx",
+    repository = "https://github.com/kprussing/secretx",
+    bugtracker = "https://github.com/kprussing/secretx/issues",
+    support = "https://github.com/kprussing/secretx/issues",
+
+    -- Package information
+    pkg = module,
+    version = "0.9",
+    summary = "LaTeX macros for importance markings",
+    ctanPath = "/macros/latex/contrib/" .. module,
+    license = "LPPL",
+}
