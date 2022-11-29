fb95a70f5b8eaf837efb36e5461d0d40ad849111
Keith F. Prussing <kprussing74@gmail.com>
2022-11-28 23:19:56 -0500

    Update the build to work with upload
    [This][1] project provides a semi-clear discussion of how to use l3build.
    It's still not completely clear, but I believe I have the core
    components listed.  This is a start for the first testing release.
    
    [1]: https://github.com/pablgonz/demopkg-jw

--- build.lua
+++ build.lua
@@ -1,6 +1,8 @@
 -- Build configuration for secretx
 
 module = "secretx"
+version = "0.10"
+pkgdate = "2022-11-28"
 
 -- The unpack directory has to be on the search path so building the
 -- documentation can find sectetx.sty
@@ -11,12 +13,19 @@ docdir = "doc/latex/" .. module .. "/"
 demofiles = {"example-doc.tex",
              "example-slides.tex"}
 typesetdemofiles = demofiles
-installfiles = {}
+docfiles = demofiles
+installfiles = {"*.sty"}
 tdslocations = {}
 for _, example in pairs(demofiles) do
     table.insert(installfiles, example)
     table.insert(tdslocations, docdir .. example)
 end
+
+-- Set the engine to get xetex and luatex to not run so it can pass
+checkengines = {"pdftex"}
+
+ctanreadme = "README.rst"
+ctanzip = module .. "-" .. version
 
 -- Set the configuration details
 uploadconfig = {
@@ -33,8 +42,9 @@ uploadconfig = {
 
     -- Package information
     pkg = module,
-    version = "0.9",
+    version = version,
     summary = "LaTeX macros for importance markings",
     ctanPath = "/macros/latex/contrib/" .. module,
-    license = "LPPL",
+    license = "LPPL1.3c",
+    topics = {"Markup", "Typesetting", "Documentation support"},
 }
